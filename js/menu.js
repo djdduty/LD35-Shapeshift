f565494a3022518d9e12016c6d3b4d97fda3a033
Jordan Duty <djdduty@gmail.com>
2016-04-15 20:28:37 -0700

    States

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+var menuState = {
+    create: function() {
+        var nameLabel = game.add.text(80, 80, 'LD35 - Shapeshift', {font: '50px Arial', fill: '#ffffff'});
+        var nameLabel = game.add.text(80, 500, 'SPACE to start', {font: '24px Arial', fill: '#ffffff'});
+        var spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spacekey.onDown.addOnce(this.start, this);
+    },
+
+    start: function() {
+        game.state.start('play');
+    }
+}
